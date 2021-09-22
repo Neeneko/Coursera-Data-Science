fdf6b73eaa3a987a0c1736aab156fb853daab482
neeneko <nathan.weyer@gmail.com>
2021-09-22 16:35:22 -0400

    week 1

---
+++ R Programming/pollutantmean.R
@@ -0,0 +1,12 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  acc <- NA
+  for(i in id)
+  {
+    path <- file.path(directory,sprintf("%03d.csv",i))
+    data = read.csv(path)
+    acc <- rbind(acc,data)
+  }
+  mean(acc[[pollutant]],na.rm=TRUE)
+}
+
+print(pollutantmean("specdata", "sulfate", 1:10))
